e8715e9dd1a5c641b75c1f00c8d5ffaff96fd01e
Fede Marinas <fedemarinasruiz@gmail.com>
2024-11-01 19:30:56 +0100

    change agency component width

--- src/components/agency-component.tsx
+++ src/components/agency-component.tsx
@@ -7,7 +7,7 @@ export const AgencyComponent = async ({ agency }: { agency: Agency }) => {
 
   return (
     <Link href={`/agency/${agency.slug}`} className="hover:scale-110 transform transition duration-200">
-      <div className="flex gap-4 flex-col justify-center items-center bg-gray-200 py-4 px-10 rounded-lg w-80 sm:w-48 h-48">
+      <div className="flex gap-4 flex-col justify-center items-center bg-gray-200 py-4 px-10 rounded-lg w-80 sm:w-40 h-40">
         {agenciesConfigurationData?.logo_url && (
           <img src={agenciesConfigurationData?.logo_url} alt="Logo de la agencia" className="w-12 object-contain" />
         )}
