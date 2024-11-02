f9e5060c755b5b766ad49fc153334d46d30d4056
Fede Marinas <fedemarinasruiz@gmail.com>
2024-11-02 07:13:14 +0100

    solve eslint

--- src/components/custom-toast.tsx
+++ src/components/custom-toast.tsx
@@ -2,13 +2,14 @@
 
 import { useToast } from "@/hooks/use-toast";
 import { Toaster } from "./ui/toaster";
-import { use, useEffect } from "react";
+import { useEffect } from "react";
 
 export const CustomToast = ({ text }: { text: string }) => {
   const { toast } = useToast();
 
   useEffect(() => {
     toast({ description: text, variant: "destructive" });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return <Toaster />;
